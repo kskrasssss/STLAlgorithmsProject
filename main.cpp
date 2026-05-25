eeb3faee59787d9180592af9fe191a37170ae499
ks.krasssss <self.developmentkkras@gmail.com>
2026-05-25 03:34:36 +0300

    Create project with includes

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <list>   
+#include <vector> 
+#include <deque>  
+#include <set>    
+#include <map>    
+#include <string> 
+#include <random> 
+#include <numeric>
+
+int main() {
+    std::cin.get();
+    return 0;
+}
